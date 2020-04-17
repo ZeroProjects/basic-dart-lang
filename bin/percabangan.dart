1c6fd5acc1e16a0329312fa9b8add897f53e0700
harrysudana <harrysudana@gmail.com>
2020-04-17 13:33:23 +0800

    L3 - Percabangan dengan switch

--- bin/percabangan.dart
+++ bin/percabangan.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 
 void main(List<String> arguments) {
   int bilangan;
+  String output;
+
   stdout.write('Masukkan bilangan? ');
   bilangan = int.tryParse(stdin.readLineSync());
   if(bilangan > 0){
@@ -11,4 +13,21 @@ void main(List<String> arguments) {
   }else{
     stdout.writeln('Bilangan nol.');
   }
+
+  output = (bilangan > 0) ? 'Bilangan Positif' : 'Negatif atau Nol';
+  stdout.writeln('Menggunakan single line percabangan : ${output}');
+
+  switch(bilangan){
+    case 0 :
+      output = 'Bilangan nol';
+      break;
+    case 1 :
+      output = 'Bilangan satu';
+      break;
+    default :
+      output = 'Bilangan selain nol dan satu';
+  }
+
+  stdout.writeln('Menggunakan switch : ${output}');
+
 }
